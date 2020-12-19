1d1a02a05c88d67dfaf798ed72210c8308fa4fed
Yohann ALVES <Yohann ALVES@DESKTOP-F06TJ8N>
2020-12-19 01:23:10 +0100

    possibilité de mettre des points dans la grille
    next step : faire en sorte de ne pas cliquer ailleurs que les endroits
    ou on peut mettre des points

--- projetJava/src/projetJava/Grid.java
+++ projetJava/src/projetJava/Grid.java
@@ -1,6 +1,8 @@
 package projetJava;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
@@ -9,7 +11,7 @@ import javax.swing.JPanel;
 public class Grid extends JPanel {
 
    int width, height, columns, rows;
-   
+   final int POINT_SIZE = 10;
    ArrayList<Point> points = new ArrayList<>();
    
    
@@ -19,30 +21,50 @@ public class Grid extends JPanel {
 	   this.setSize(this.width, this.height);
 	   this.columns = cols;
 	   this.rows = rws;
+	   
    }
    
-   public void paint(Graphics g) {
-	   g.setColor(Color.gray);
+   
+   @Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		  Graphics2D g2d = (Graphics2D) g; // en graphics2d pour permettre de zoomer (scale)
+		  this.draw(g2d);
+		 
+		   
+	}
+ 
+  
+
+
+   public void draw(Graphics2D g2d) {
+	   g2d.scale(3,3);
+	   g2d.setColor(Color.gray);
 	   //draw rows
 	   for(int i = 0; i < rows; i++) {
-		   g.drawLine(0, i*(height/rows), width, i*(height/rows));
+		   g2d.drawLine(0, i*(height/rows), width, i*(height/rows));
 	   }
 	   
 	   //draw columns
 	   for (int i =0; i<columns; i++) {
-		   g.drawLine(i*(height/rows), 0, i*(height/rows), height);
+		   g2d.drawLine(i*(height/rows), 0, i*(height/rows), height);
 	   }
+	   g2d.setColor(Color.black);
 	   
-	   this.pointDraw(g);
+	   
+	   this.addMouseListener(new MouseAdapter() { // bug : quand resize de fenetre ==> points disparaissent
+ 		     @Override
+ 		     public void mousePressed(MouseEvent e) {
+ 		    	getGraphics().fillOval(e.getX(), e.getY(), POINT_SIZE, POINT_SIZE); //créer un point au clic
+ 		    	
+ 		        
+ 		     }
+ 		    
+ 		  });
+	   
    }
+  
 
-   public void pointDraw(Graphics g) {
-	   Point p = new Point(rows-5,columns-3);
-	   g.setColor(Color.black);
-	   g.fillOval(p.getX(), p.getY(), 10, 10);//coordonées, taille du point
-   }
-   
-   
 
 }
 
--- projetJava/src/projetJava/Main.java
+++ projetJava/src/projetJava/Main.java
@@ -7,13 +7,21 @@ import javax.swing.JFrame;
 public class Main extends JFrame{
 
     public static void main(String[] a) {		
-                Grid g = new Grid(500,500,20,20); //taille de la grille, colonnes, lignes
+    	
+        		
+        		
+        		
+                Grid grid = new Grid(500,500,50,50); //taille de la grille, colonnes, lignes
                 JFrame f = new JFrame();
                 f.setPreferredSize(new Dimension(500, 500));
     			f.setLocationRelativeTo(null);
-                f.add(g);
+                f.add(grid);
                 f.pack();
                 f.setVisible(true);    
+                
+                
+                
+                
             }
         
     }
